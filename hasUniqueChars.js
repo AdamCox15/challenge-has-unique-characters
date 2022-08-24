34f78472bcc05a6b1df8f9aff591c0fe80d4695a
Adam Cox <adamcox.dev@gmail.com>
2022-08-24 10:05:02 -0600

    Second and final commit for coding challenge

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -1,1 +1,14 @@
 // Write your code below
+function thisThing(word){
+    let wordArray = word.split('');
+for(let i = 0; i < word.length; i++){
+    for(let j =0; j < word.length; j++){
+    if(i !== j && wordArray[i] === wordArray[j]){
+        return false;
+    }
+}
+}
+return true;
+}
+
+console.log(thisThing('Mann'));
